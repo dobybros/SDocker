067814b12d094fedeb70721f910be72270852f92
huzhanjing <372481643@qq.com>
2017-10-27 20:47:39 +0800

    ssim去掉-v

--- src/main/java/DeploySSIMService.java
+++ src/main/java/DeploySSIMService.java
@@ -7,7 +7,7 @@ public class DeploySSIMService {
         String serviceName = "ssim";
         String gridfsHost = "mongodb://localhost:7900";
 //        String gridfsHost = "mongodb://poker.9spirit.cn:7900";
-        String version = "1";
-        DeployServiceUtils.main(new String[]{"-p", servicePath, "-d", dockerName, "-s", serviceName, "-f", gridfsHost, "-v", version});
+//        String version = "1";
+        DeployServiceUtils.main(new String[]{"-p", servicePath, "-d", dockerName, "-s", serviceName, "-f", gridfsHost});
     }
 }
