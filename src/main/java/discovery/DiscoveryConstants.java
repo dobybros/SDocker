90fbb6da26db642f9eda07c4fe704454ae3eea67
Aplomb <aplomb.chen@gmail.com>
2017-11-13 17:47:16 +0800

    clean up project.

---
+++ src/main/java/discovery/DiscoveryConstants.java
@@ -0,0 +1,8 @@
+package discovery;
+
+public interface DiscoveryConstants {
+    String PATH = "/home/aplomb/dev/projects/sdocker/";
+    String DOCKERNAME = "docker";
+    String GRIDFSHOST = "mongodb://localhost:7900";
+    String PREFIX = "scripts";
+}
