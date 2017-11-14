10761b27b17bfc930843dd528a0e84847ca814fa
huzhanjing <372481643@qq.com>
2017-11-14 19:54:25 +0800

    添加SSCS

---
+++ src/main/java/sscs/SSCSConstants.java
@@ -0,0 +1,8 @@
+package pk;
+
+public interface PKConstants {
+    String PATH = "/Users/admin/workSpace/manageservers/";
+    String DOCKERNAME = "docker";
+    String GRIDFSHOST = "mongodb://localhost:7900";
+    String PREFIX = "scripts";
+}
