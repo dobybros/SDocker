3dae67b5cbf6ed0cac6fbd9bc284cbb30bf32add
huzhanjing <372481643@qq.com>
2017-11-21 15:50:02 +0800

    no message

--- src/main/java/im/DeployGWCustomerService.java
+++ src/main/java/im/DeployGWCustomerService.java
@@ -2,15 +2,15 @@ package im;
 
 import com.docker.utils.DeployServiceUtils;
 
-public class DeployIMAgencyService {
+public class DeployGWCustomerService {
     public static void main(String[] args) throws Exception {
-        String servicePath = IMConstants.PATH + "IMAgencyService";
+        String servicePath = IMConstants.PATH + "GWCustomerService";
         String dockerName = "gateway";
-        String serviceName = "imagency";
+        String serviceName = "gwcustomer";
         String gridfsHost = IMConstants.GRIDFSHOST;
 //        String gridfsHost = "mongodb://poker.9spirit.cn:7900";
-        String version = "1";
+//        String version = "1";
         String prefix = IMConstants.PREFIX;
-        DeployServiceUtils.main(new String[]{"-x", prefix, "-p", servicePath, "-d", dockerName, "-s", serviceName, "-f", gridfsHost, "-v", version});
+        DeployServiceUtils.main(new String[]{"-x", prefix, "-p", servicePath, "-d", dockerName, "-s", serviceName, "-f", gridfsHost});
     }
 }
