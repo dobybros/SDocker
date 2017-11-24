1dd5f2b2d847e8237794842bbf8f6333b6153851
huzhanjing <372481643@qq.com>
2017-11-24 18:16:30 +0800

    修改路径

--- src/main/java/sscs/DeploySSCSService.java
+++ src/main/java/sscs/DeploySSCSService.java
@@ -6,11 +6,11 @@ import im.IMConstants;
 public class DeploySSCSService {
     public static void main(String[] args) throws Exception {
         String servicePath = SSCSConstants.PATH + "SSCustomerStorageService";
-        String dockerName = IMConstants.DOCKERNAME;
+        String dockerName = SSCSConstants.DOCKERNAME;
         String serviceName = "sscustomer";
-        String gridfsHost = IMConstants.GRIDFSHOST;
+        String gridfsHost = SSCSConstants.GRIDFSHOST;
         String version = "1";
-        String prefix = IMConstants.PREFIX;
+        String prefix = SSCSConstants.PREFIX;
         DeployServiceUtils.main(new String[]{"-x", prefix, "-p", servicePath, "-d", dockerName, "-s", serviceName, "-f", gridfsHost, "-v", version});
     }
 }
