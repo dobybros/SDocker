33647b4980988a7b36ca897caeea62b09eded27c
Aplomb <aplomb.chen@gmail.com>
2017-11-29 14:16:30 +0800

    Commit IMRobot deploy file and template deploy file.

---
+++ src/main/java/template/TemplateConstants.java
@@ -0,0 +1,8 @@
+package template;
+
+public interface TemplateConstants {
+    String PATH = "/home/aplomb/dev/projects/template/";
+    String DOCKERNAME = "docker";
+    String GRIDFSHOST = "mongodb://localhost:7900";
+    String PREFIX = "scripts";
+}
