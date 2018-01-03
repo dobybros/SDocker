0f8fb6d3949780cf801207e12eb3ebf40c315e88
huzhanjing <372481643@qq.com>
2018-01-03 11:03:45 +0800

    no message

--- src/main/java/pk/PKConstants.java
+++
@@ -1,9 +0,0 @@
-package pk;
-
-public interface PKConstants {
-    String PATH = "/Users/admin/workSpace/manageservers/";
-    String DOCKERNAME = "docker";
-    String GRIDFSHOST = "mongodb://localhost:7900";
-//    String GRIDFSHOST = "mongodb://poker.9spirit.cn:7900";
-    String PREFIX = "scripts";
-}
