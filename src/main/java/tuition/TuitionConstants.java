d6b550bf90f2e52886fb5e90ee6eb3aaf2236cff
licklulu <lick1244036947@163.com>
2019-01-03 15:39:39 +0800

    1

--- src/main/java/tuition/TuitionConstants.java
+++
@@ -1,8 +0,0 @@
-package tuition;
-
-public interface TuitionConstants {
-    String PATH = "C:\\Users\\lulia\\work\\work_new\\TuitionCloud\\";
-    String DOCKERNAME = "docker_tc";
-    String GRIDFSHOST = "mongodb://192.168.2.184:7900";
-    String PREFIX = "scripts";
-}
